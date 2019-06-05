259e9bda4f6ca03890db3ffb543d4edf0fe32d7e
谢宝伟 <48127165+xiebaowei@users.noreply.github.com>
2019-06-05 22:38:24 +0800

    Create QueryVo.java
    实体类作为参数，新建类，为之后开发做铺垫

---
+++ QueryVo.java
@@ -0,0 +1,16 @@
+package com.itheima.springmvc.pojo;
+
+public class QueryVo {
+
+
+	private Memberships memberships;
+
+	public Memberships getMemberships() {
+		return memberships;
+	}
+
+	public void setMemberships(Memberships memberships) {
+		this.memberships = memberships;
+	}
+	
+}
